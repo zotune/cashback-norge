6550eb258bbaf9ae7002d32cf88c620daa7a254f
zotune <zotune@gmail.com>
2026-05-06 03:16:46 +0200

    Crawl

--- site/cashback-varsler.user.js
+++ site/cashback-varsler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cashbacknorge.no
 // @namespace    https://cashbacknorge.no/
-// @version      1778029438
+// @version      1778030167
 // @description  Vis cashback-tilbud automatisk på norske nettbutikker
 // @author       zotune
 // @icon         https://cashbacknorge.no/favicon.png
