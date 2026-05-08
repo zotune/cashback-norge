049d6182556a763fdd178447962709156329826f
zotune <zotune@gmail.com>
2026-05-08 17:42:08 +0200

    Rebuild extension/userscript with spenn provider fix

--- site/cashback-varsler.user.js
+++ site/cashback-varsler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cashbacknorge.no
 // @namespace    https://cashbacknorge.no/
-// @version      1778254690
+// @version      1778254921
 // @description  Vis cashback-tilbud automatisk på norske nettbutikker
 // @author       zotune
 // @icon         https://cashbacknorge.no/favicon.png
